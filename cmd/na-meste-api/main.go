b5f7da1ef4a618d47dfa2791fea56b2197203d27
bra1ns00 <godlevskiy.is.working@gmail.com>
2025-02-06 18:29:57 +0300

    Added a logger

--- cmd/na-meste-api/main.go
+++ cmd/na-meste-api/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/cyberbrain-dev/na-meste-api/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	logger := setupLogger(cfg.Env)
 
-	// TODO: setup logger
+	logger.Info("Hello")
 
 	// TODO: init database
+}
+
+// Sets up a slog logger
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
