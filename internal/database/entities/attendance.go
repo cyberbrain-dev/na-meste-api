853820dd4b0d3e6816a4af9ba4873e785285b9ac
bra1ns00 <godlevskiy.is.working@gmail.com>
2025-02-22 17:10:45 +0300

    Implemented an attendances Repository

--- internal/database/entities/attendance.go
+++ internal/database/entities/attendance.go
@@ -5,7 +5,7 @@ import "time"
 // Represents an attendance record in th db
 type Attendance struct {
 	ID        uint      `gorm:"primaryKey"`
-	UserID    uint      `gorm:"->;not null;constraint:OnDelete:CASCADE;"`
-	CollegeID uint      `gorm:"->;not null;constraint:OnDelete:CASCADE;"`
+	UserID    uint      `gorm:"<-:create;not null;constraint:OnDelete:CASCADE;"`
+	CollegeID uint      `gorm:"<-:create;not null;constraint:OnDelete:CASCADE;"`
 	Date      time.Time `gorm:"not null;index"`
 }
